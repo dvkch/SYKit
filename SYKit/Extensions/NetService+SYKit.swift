18a70bf49534a4d6bc591e678d5f7c85c8425c4c
Stanislas Chevallier <contact@stanislaschevallier.fr>
2023-05-17 17:34:04 +0200

    Update NetService again, bump to 0.2.4

--- SYKit/Extensions/NetService+SYKit.swift
+++ SYKit/Extensions/NetService+SYKit.swift
@@ -8,58 +8,74 @@
 
 import Foundation
 
-public extension NetService {
-    class Address {
-        public enum Kind {
-            case ip4
-            case ip6
-        }
-        
-        public let kind: Kind
-        public let ip: String
-        public let port: Int
-        
-        public init(kind: Kind, ip: String, port: Int) {
-            self.kind = kind
-            self.ip = ip
-            self.port = port
-        }
+public class NetAddress {
+    public enum Family {
+        case ip4
+        case ip6
     }
-
-    var parsedAddresses: [Address]? {
-        return addresses?.compactMap { $0.ipAddress }
+    
+    public let family: Family
+    public let ip: String
+    public let port: Int
+    public let host: String
+    public let serv: String
+    
+    public init(family: Family, ip: String, port: Int, host: String, serv: String) {
+        self.family = family
+        self.ip = ip
+        self.port = port
+        self.host = host
+        self.serv = serv
     }
 }
 
-private extension Data {
-    var ipAddress: NetService.Address? {
+public extension NetService {
+    var parsedAddresses: [NetAddress]? {
+        return addresses?.compactMap { $0.netAddress }
+    }
+}
+
+public extension Data {
+    var netAddress: NetAddress? {
+        var ipBuffer   = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+        var portBuffer = [CChar](repeating: 0, count: Int(NI_MAXSERV))
         var hostBuffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
         var servBuffer = [CChar](repeating: 0, count: Int(NI_MAXSERV))
-        var kind: NetService.Address.Kind = .ip4
+        var family: NetAddress.Family = .ip4
 
         let err = withUnsafeBytes { buf -> Int32 in
             let sa = buf.baseAddress!.assumingMemoryBound(to: sockaddr.self)
 
             if sa.pointee.sa_family == AF_INET {
-                kind = .ip4
+                family = .ip4
             }
 
             if sa.pointee.sa_family == AF_INET6 {
-                kind = .ip6
+                family = .ip6
             }
 
-            return getnameinfo(
+            let err1 = getnameinfo(
+                sa, socklen_t(sa.pointee.sa_len),
+                &ipBuffer, socklen_t(ipBuffer.count),
+                &portBuffer, socklen_t(portBuffer.count),
+                NI_NUMERICHOST | NI_NUMERICSERV
+            )
+
+            let err2 = getnameinfo(
                 sa, socklen_t(sa.pointee.sa_len),
                 &hostBuffer, socklen_t(hostBuffer.count),
                 &servBuffer, socklen_t(servBuffer.count),
-                NI_NUMERICHOST | NI_NUMERICSERV
+                0
             )
+            
+            return err1 > 0 ? err1 : err2
         }
         guard err == 0 else { return nil }
 
+        let ip   = String(cString: hostBuffer)
+        let port = String(cString: portBuffer)
         let host = String(cString: hostBuffer)
-        guard let port = Int(String(cString: servBuffer)) else { return nil }
-        
-        return .init(kind: kind, ip: host, port: port)
+        let serv = String(cString: servBuffer)
+        return .init(family: family, ip: ip, port: Int(port) ?? 0, host: host, serv: serv)
     }
 }
