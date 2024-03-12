1743887959acf2f017a3326e11b7ebab4cfafe80
Stanislas Chevallier <contact@stanislaschevallier.fr>
2024-03-12 11:37:33 +0100

    Improve Package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SYKit",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SYKit",
-            targets: ["SYKit"]),
+        .library(name: "SYKit", targets: ["SYKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SYKit"),
-        .testTarget(
-            name: "SYKitTests",
-            dependencies: ["SYKit"]),
+        .target(name: "SYKit"),
+        .testTarget(name: "SYKitTests", dependencies: ["SYKit"]),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
